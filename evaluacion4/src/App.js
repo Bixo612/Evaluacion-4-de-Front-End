7744978bf9de1111718c10029905323d6e8d4ea1
Bixo612 <vicente.vasquez612@gmail.com>
2022-07-23 18:35:39 -0400

    -

--- evaluacion4/src/App.js
+++ evaluacion4/src/App.js
@@ -4,7 +4,6 @@ import ListadoResultados from './listadoProductos';
 import { useState } from 'react';
 import './App.css';
 
-
 function App() {
 
   //useState inicializa con arreglo vacío la  variable de estado operaciones
@@ -13,20 +12,39 @@ function App() {
   function agregarCarro(event) {
     //prevent default evita que el submit envie los vaores a otra pagina y taget los pase a la funcion
     event.preventDefault();
-    const codigo = event.target.txt_codigo.value
-    const articulo = event.target.txt_articulo.value
+    const codigo = event.target.txt_codigo.value.toUpperCase();
+    const articulo = event.target.sel_articulo.value
     const descripicion = event.target.txt_descripicion.value
     const precio = parseFloat(event.target.txt_precio.value)
-    const cantidad = parseFloat(event.target.txt_cantidad.value)
+    const cantidad = parseFloat(event.target.num_cantidad.value)
     const total = cantidad * precio
-    const totalDescuento = total * 0.9
-    /*
-    const lado_a = parseFloat(event.target.txt_valor_a.value)
-    const lado_b = parseFloat(event.target.txt_valor_b.value)
-    const area = lado_a * lado_b
-    */
-    //se crea un objeti neuvo con los resultados y datos obtenidos del formulario
-
+    const totalDescuento = Math.trunc(total * 0.9)
+    //validaciones
+    if (codigo.length < 3 || codigo.length > 6) {
+      alert("El codigo debe contener entre 3 y 6 caracteres")
+      return false;
+    }
+    if (articulo == "") {
+      alert("Debe selecionar un articulo")
+      return false;
+    }
+    if (descripicion == "") {
+      alert("Debe ingresar una descripicion")
+      return false;
+    }
+    if (isNaN(precio)) {
+      alert("El precio debe ser ingresado en numeros")
+      return false;
+    }
+    if (precio <= 0) {
+      alert("El precio debe ser mayor a 0")
+      return false;
+    }
+    if (cantidad <= 0) {
+      alert("la cantidad debe ser 1 o mas")
+      return false;
+    }
+    //se crea un objeto nuevo con los resultados y datos obtenidos del formulario
     const nuevo = {
       codigo_: codigo,
       articulo_: articulo,
@@ -35,23 +53,14 @@ function App() {
       cantidad_: cantidad,
       total_: total,
       totalDescuento_: totalDescuento
-      /*
-      valor1: lado_a,
-      valor2: lado_b,
-      resultado: area
-      */
     }
     //modifica el estado de useState con el nuevo objeto
     setOperacion([...operaciones, nuevo])
     event.target.txt_codigo.value = ''
-    event.target.txt_articulo.value = ''
+    event.target.sel_articulo.value = ''
     event.target.txt_descripicion.value = ''
     event.target.txt_precio.value = ''
-    event.target.txt_cantidad.value = ''
-    /*
-    event.target.txt_valor_a.value = ''
-    event.target.txt_valor_b.value = ''
-    */
+    event.target.num_cantidad.value = ''
   }
 
   return (
@@ -67,69 +76,59 @@ function App() {
               {/* Codigo */}
               <label for="txt_codigo">Codigo</label>
               <input type="text" name="txt_codigo" id="txt_codigo" className="form-control" />
+              <div id="emailHelp" className="form-text">Debe contener entre 3 y 6 caracteres</div>
               {/* Articulo */}
-              <label for="txt_articulo">Articulo</label>
-              <input type="text" name="txt_articulo" id="txt_articulo" className="form-control" />
+              <label for="sel_articulo">Articulo</label>
+              <select className="form-select" name="sel_articulo" id="sel_articulo">
+                <option selected value="">Selecione articulo</option>
+                <option value="Zapatillas">Zapatillas</option>
+                <option value="Zapatos">Zapatos</option>
+                <option value="Botas">Botas</option>
+                <option value="Sandalias">Sandalias</option>
+              </select>
               {/* Descripcion */}
               <label for="txt_descripicion">Descripcion</label>
               <input type="text" name="txt_descripicion" id="txt_descripicion" className="form-control" />
-              {/* Precio */}
-              <label for="txt_precio">Precio</label>
-              <input type="text" name="txt_precio" id="txt_precio" className="form-control" />
-              {/* Cantidad */}
-              <label for="txt_cantidad">Cantidad</label>
-              <input type="number" name="txt_cantidad" id="txt_cantidad" className="form-control" />
+              <div className="row">
+                <div class="col">
+                {/* Precio */}
+                <label for="txt_precio">Precio</label>
+                <input type="text" name="txt_precio" id="txt_precio" className="form-control" placeholder='$' />
+                </div>
+                <div class="col">
+                {/* Cantidad */}
+                <label for="num_cantidad">Cantidad</label>
+                <input type="number" name="num_cantidad" id="num_cantidad" className="form-control" placeholder='-' />
+                </div>
+              </div>
               {/* Total */}
               {/* Total descuento */}
               <br />
-              <input type="submit" className='btn btn-primary' value="Calcular área rectangulo" />
+              <div className="btn-group" role="group" aria-label="Basic example">
+                <input type="submit" className='btn btn-success' value="Agregar al carro" />
+                <input type="reset" className='btn btn-danger' value="Limpiar" />
+              </div>
             </form>
           </div>
-          <div className="col-xs-12 col-sm-6">
-            <table className="table">
-              <thead>
-                <tr>
-                  <th scope="col">Codigo</th>
-                  <th scope="col">Articulo</th>
-                  <th scope="col">Descripcion</th>
-                  <th scope="col">Precio</th>
-                  <th scope="col">Cantidad</th>
-                  <th scope="col">Total</th>
-                  <th scope="col">Con descuento</th>
-                </tr>
-              </thead>
-                <ListadoResultados resultados={operaciones} />
-            </table>
-          </div>
         </div>
+        <hr />
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Codigo</th>
+              <th scope="col">Articulo</th>
+              <th scope="col">Descripcion</th>
+              <th scope="col">Precio</th>
+              <th scope="col">Cantidad</th>
+              <th scope="col">Total</th>
+              <th scope="col">Total descuento</th>
+            </tr>
+          </thead>
+          <ListadoResultados resultados={operaciones} />
+        </table>
       </body>
     </div>
   );
-
-  /*
-      return (
-          <div> 
-              <div className='container'>
-                  <form onSubmit={agregarCarro}>
-  
-                      <h2>Cálculo área rectangulo</h2>
-                      <div className='row'>
-                          <div className='col-xs-12 col-sm-6'>
-                              Ingrese lado A:
-                              <input className='form-control' type="text" name='txt_valor_a' /><br />
-                          </div>
-                          <div className='col-xs-12 col-sm-6'>
-                              Ingrese lado B:
-                              <input className='form-control' type="text" name='txt_valor_b' /><br />
-                          </div>
-                          <input type="submit" className='btn btn-primary' value="Calcular área rectangulo" />
-                      </div>                    
-                      <ListadoResultados resultados={operaciones} />
-                  </form>
-              </div>
-  
-          </div>
-      );*/
 }
 
 export default App;
--- evaluacion4/src/listadoProductos.js
+++ evaluacion4/src/listadoProductos.js
@@ -1,15 +1,15 @@
 function ListadoResultados(prop) {
     return (
-        <tbody>
+        <tbody className="table-group-divider">
             {prop.resultados.map((elemento) =>
                 <tr>
                     <th> {elemento.codigo_} </th>
                     <td> {elemento.articulo_} </td>
                     <td> {elemento.descripicion_} </td>
-                    <td> ${elemento.precio_}</td>
+                    <td> $ {elemento.precio_}</td>
                     <td> {elemento.cantidad_}</td>
-                    <td> ${elemento.total_}</td>
-                    <td> ${elemento.totalDescuento_}</td>
+                    <td> $ {elemento.total_}</td>
+                    <td> $ {elemento.totalDescuento_}</td>
                 </tr>
             )
             }
@@ -17,13 +17,3 @@ function ListadoResultados(prop) {
     );
 }
 export default ListadoResultados;
-
-
-/*
-        <ol>
-            {prop.resultados.map((elemento) =>
-                <li>Lado A:{elemento.valor1} Lado B: {elemento.valor1} Área: {elemento.resultado}</li>
-            )
-            }
-        </ol>
-*/
